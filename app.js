13d5a885b04a3c40c789c1207767f3183ac66a25
Suraj Thapa <surajthapart@gmail.com>
2024-07-17 17:13:15 +0545

    Installed and used bcript and jwt tokenand also cookies

--- app.js
+++ app.js
@@ -2,7 +2,12 @@ import cookieParser from 'cookie-parser';
 import express from 'express';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken'
+
 import User from './models/userModel.js';
+
+
 
 // Needed to use `__dirname` with ES modules
 const __filename = fileURLToPath(import.meta.url);
@@ -20,22 +25,31 @@ app.use(cookieParser());
 app.get('/', (req, res) => {
     res.render('index');
 });
-app.post('/register', async (req, res) => {
+app.post('/register',  (req, res) => {
     const { name, age, email, password } = req.body;
-    try {
-        const newUser = await User.create({
-            name,
-            age,
-            email,
-            password
-        });
-        res.send(newUser);
-        console.log(newUser)
-    } catch (err) {
-        console.error(err);
-        res.status(500).send('Error registering user');
-    }
+    bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(password, salt, async (err, hash) => {
+            const newUser = await User.create({
+                name,
+                age,
+                email,
+                password: hash
+            });
+
+            let token = jwt.sign({email}, "token")
+            res.cookie("token", token)
+            res.redirect('/');
+            console.log(token)
+        })
+        
+        
+    })
 });
+
+app.get('/logout', (req, res) => {
+    res.cookie("token","")
+    res.redirect('/');
+})
 
 app.listen(3000, () => {
     console.log('Server is running on http://localhost:3000');
