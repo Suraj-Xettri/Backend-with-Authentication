46d0ed14eae834c37256ac71d65ca1380510eed1
Suraj Thapa <surajthapart@gmail.com>
2024-07-21 21:11:23 +0545

    added image in user model

--- models/userModel.js
+++ models/userModel.js
@@ -14,6 +14,10 @@ const userSchema = new mongoose.Schema({
     password: {
         type: String,
     },
+    image:{
+        type: String,
+        default: "unknown.jpg"
+    },
     posts: [{
         type: mongoose.Schema.Types.ObjectId,
         ref: 'Post'
